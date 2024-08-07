555da98fc13593c5f794a1a86aae974f1109fc8a
SCross-hub <scrpd@allstate.com>
2024-08-07 15:34:05 -0400

    load balancer v2

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "alb" {
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = [module.blog_sg.security_group_id]
 
   access_logs = {
     bucket = "my-alb-logs"
@@ -74,7 +74,7 @@ module "alb" {
           target_id = aws_instance.blog.id
           port = 80
          }
-        }
+        },
       ]
     }
   ]

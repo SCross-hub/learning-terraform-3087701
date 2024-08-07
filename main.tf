dafe80e36f1a1b568e7b142d47f67faec1eb7879
SCross-hub <scrpd@allstate.com>
2024-08-07 16:08:33 -0400

    autoscaling v3

--- main.tf
+++ main.tf
@@ -34,21 +34,21 @@ module "blog_vpc" {
   }
 }
 
-  module "autoscaling" {
-    source  = "terraform-aws-modules/autoscaling/aws"
-    version = "6.5.2"
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-    name     = "blog"
-    min_size = 1
-    max_size = 2
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-    vpc_zone_identifier = module.blog_vpc.public_subnets
-    target_group_arns   = module.blog_alb.target_group_arns
-    security_groups = [module.blog_sg.security_group_id]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
  
-    image_id      = data.aws_ami.app_ami.id
-    instance_type = var.instance_type
-  }
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+}
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -58,22 +58,28 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   access_logs = {
     bucket = "my-alb-logs"
   }
 
- target_groups = [
-  {
-    name_prefix      = "blog-"
-    backend_protocol = "HTTP"
-    backend_port     = 80
-    target_type      = "instance"
-  }
-]
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      ]
+    }
+  ]
 
   http_tcp_listeners = [
     {
@@ -90,12 +96,13 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2" 
+  version = "5.1.2"
+  
   name = "blog"
 
   vpc_id = module.blog_vpc.vpc_id
-  ingress_rules           = ["http-80-tcp","https-443-tcp"]
+  ingress_rules           = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks     = ["0.0.0.0/0"]
   egress_rules            = ["all-all"]
-  egress_cidr_blocks      = ["0.0.0.0/0"] 
+  egress_cidr_blocks      = ["0.0.0.0/0"]
 }

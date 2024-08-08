653364d22bc9f154fe8980acbe2deb8785086618
SCross-hub <scrpd@allstate.com>
2024-08-08 12:28:06 -0400

    idk

--- qa/main.tf
+++ qa/main.tf
@@ -1,11 +1,9 @@
 module "qa" {
   source = "../modules/blog"
-
   environment = {
-      name           = "qa"
-      network_prefix = "10.1"
+    name = "qa"
+    network_prefix = "10.1"
   }
-
   asg_min = 0
   asg_max = 0
 }
